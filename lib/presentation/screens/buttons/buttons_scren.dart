345399494ececd113873c52c6e3c6a0c84f4692a
Francisco Colmenarez <frncsico@gmail.com>
2024-02-01 13:13:30 -0400

    126 - se creo el custom button

--- lib/presentation/screens/buttons/buttons_scren.dart
+++ lib/presentation/screens/buttons/buttons_scren.dart
@@ -102,9 +102,36 @@ class _ButtonsView extends StatelessWidget {
                 backgroundColor: MaterialStatePropertyAll(colors.primary),
                 iconColor: const MaterialStatePropertyAll(Colors.white),
               ),
-            )
+            ),
+
+            const CustomButton(),
 
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: (){},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+            child: Text('Button Custom', style: TextStyle(color: Colors.white),)
+          ),
         ),
       ),
     );
