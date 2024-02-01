8ef1ba0cff609495b6544843823c6da25fdd79a2
Francisco Colmenarez <frncsico@gmail.com>
2024-02-01 17:11:05 -0400

    130 - screens creadas y barril actualizado

---
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AnimateScreen extends StatelessWidget {
+  
+  static const name = 'animated_screen';
+
+  const AnimateScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
