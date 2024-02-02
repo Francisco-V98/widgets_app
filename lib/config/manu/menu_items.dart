dfe9ff4ec1460ac99a276a9b639dc5070ffb95d7
Francisco Colmenarez <frncsico@gmail.com>
2024-02-02 14:22:21 -0400

    138 -  se activo la pantalla snackbar y se le agrego un btnFloat que dispara un snack

--- lib/config/manu/menu_items.dart
+++ lib/config/manu/menu_items.dart
@@ -37,4 +37,11 @@ const appMenuItems = <MenuItem>[
     icon: Icons.refresh_rounded
   ),
 
+  MenuItem(
+    title: 'Snackbars y dialogos', 
+    subTitle: 'Indicadores en pantallas', 
+    link: '/snackbars', 
+    icon: Icons.info_outline
+  ),
+
 ];
--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -6,8 +6,31 @@ class SnackbarScreen extends StatelessWidget {
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar(BuildContext context){
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackbar = SnackBar(
+      content: const Text('Hola mundo'),
+      action: SnackBarAction(label: 'Acepto', onPressed: () {}),
+      duration: const Duration(seconds: 2),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbars y Dialogos'),
+      ),
+
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Mostrar Snackbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () => showCustomSnackbar(context), 
+      ),
+    );
   }
 }
