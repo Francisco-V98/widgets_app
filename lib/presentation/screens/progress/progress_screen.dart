b89684a2dbcdd637220b15327da9a767556ae368
Francisco Colmenarez <frncsico@gmail.com>
2024-02-02 13:03:42 -0400

    135- Inicio seccion 10, progressIndicator, circle progress

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -8,6 +8,46 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Progress Indicators'),
+      ),
+      body: const _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        children: [
+          SizedBox(height: 30),
+          Text('Circulo progress indicator'),
+          SizedBox(height: 10),
+          CircularProgressIndicator(strokeWidth: 2,backgroundColor: Colors.black12),
+
+          SizedBox(height: 20),
+          Text('Progress indicator controlado'),
+          SizedBox(height: 10),
+          _ControlledProgressIndicator(),
+
+
+        ],
+      ),
+    );
+  }
+}
+
+
+class _ControlledProgressIndicator extends StatelessWidget {
+  const _ControlledProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
     return Container();
   }
 }
